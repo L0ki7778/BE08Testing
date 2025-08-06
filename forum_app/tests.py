bd2b5023de66ef1ab6e74293e3018544ab922681
Rene Heller <128136936+L0ki7778@users.noreply.github.com>
2025-08-06 06:30:05 +0200

    setting up fist url tests

--- forum_app/tests.py
+++ forum_app/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+class LikeTest(APITestCase):
+    
+    def test_get_like(self):
+        url = reverse('like-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_get_static_url_like(self):
+        url = 'http://127.0.0.1:8000/api/forum/likes/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_get_answers(self):
+        url = reverse('answer-list-create')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
